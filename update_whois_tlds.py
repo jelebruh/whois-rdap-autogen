e0788d8a1040dee8c41b68c4639a9cd3eea3e503
Dm Tenevoy <90205154+jelebruh@users.noreply.github.com>
2025-11-26 00:11:56 +0300

    Refactor update_whois_tlds.py to remove RDAP handling
    Removed redundant RDAP fetching functions and logging.

--- update_whois_tlds.py
+++ update_whois_tlds.py
@@ -9,19 +9,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("A")
 
 
-def simple(url):
-    res = {}
-    with rq.urlopen(url) as resp:
-        dns = json.loads(resp.read())
-
-    for serv in dns["services"]:
-        domains = serv[0]
-        servers = serv[1]
-        for d in domains:
-            res[d] = servers
-    return res
-
-
 def main():
     whois = {}
 
@@ -49,7 +36,6 @@ def main():
         if w:
             w = whois[domain] = w.group(1)
         logger.info(f"WHOIS: {w}\n")
-
     with open("whois.json", "w") as f:
         whois = dict(sorted(whois.items()))
         with open("whois_sld.json") as s:
@@ -57,30 +43,5 @@ def main():
         json.dump(whois, f, indent=4)
     logger.info("WHOIS dumped.")
 
-    logger.info("Getting RDAP Bootstrap DNS...")
-    with open("rdap.json", "w") as f:
-        json.dump(simple("https://data.iana.org/rdap/dns.json"), f)
-
-    logger.info("Getting RDAP Bootstrap IPv4...")
-    with open("ipv4.json", "w") as f:
-        json.dump(simple("https://data.iana.org/rdap/ipv4.json"), f)
-
-    logger.info("Getting RDAP Bootstrap IPv6...")
-    with open("ipv6.json", "w") as f:
-        json.dump(simple("https://data.iana.org/rdap/ipv6.json"), f)
-
-    logger.info("Getting RDAP Bootstrap ASNs...")
-    with rq.urlopen("https://data.iana.org/rdap/asn.json") as response:
-        asns = json.loads(response.read())["services"]
-    res = {}
-    for asn in asns:
-        nums, servs = asn
-        for n in nums:
-            for x in range(*map(int, n.split("-"))):
-                res[str(x)] = servs
-    res = dict(sorted(res.items(), key=lambda item: int(item[0])))
-    with open("asns.json", "w") as f:
-        json.dump(res, f)
-    logger.info("Done.")
 
 main()
